4bcc586ae21c29d60f115c109c9bc655b8d6ea26
Khaled Mohamed <khaledmhmd174@gmail.com>
2020-11-04 01:00:33 +0200

    fixed some minor bugs in the code

--- script.js
+++ script.js
@@ -5,32 +5,46 @@ const result = document.getElementsByClassName("result")[0];
 const highscore = document.getElementsByClassName("highscore")[0];
 const score = document.getElementsByClassName("score")[0];
 const randomNumber = Math.floor(Math.random() * 20) + 1;
-let x = 20;
+let x = 5;
 
-console.log(randomNumber);
 checkButton.addEventListener("click", compareNumbers);
 function compareNumbers() {
   let value = Number(inputNumber.value);
   if (value >= 1 && value <= 20) {
     if (value === randomNumber) {
       result.innerHTML = "You are CORRECT!";
-      let trials = 21 - x;
+      let trials = 6 - x;
       score.innerHTML = `Congratulations! You guessed it after ${trials} times!`;
       document.body.style.backgroundColor = "#03fc5e";
       document.getElementById("question").innerHTML = randomNumber;
       inputNumber.style.backgroundColor = "#03fc5e";
     } else if (value > randomNumber) {
-      result.innerHTML = "That's a little bit high :( Try again!";
-      x = x - 1;
-      score.innerHTML = "Score: " + x;
+      if (x > 1) {
+        result.innerHTML = "That's a little bit high :( Try again!";
+        x = x - 1;
+        score.innerHTML = "You have " + x + " guesses left.";
+      } else {
+        result.innerHTML = "You didn't guess the correct number. Try again!";
+        score.innerHTML = "You have 0 guesses left.";
+      }
     } else if (value < randomNumber) {
-      result.innerHTML = "That's a little bit low :( Try again!";
-      x = x - 1;
-      score.innerHTML = "Score: " + x;
+      if (x > 1) {
+        result.innerHTML =
+          "You didn't guess the correct number. Press AGAIN button to try again!";
+        x = x - 1;
+        score.innerHTML = "You have " + x + " guesses left.";
+      } else {
+        result.innerHTML =
+          "You didn't guess the correct number. Press AGAIN button to try again!";
+        score.innerHTML = "You have 0 guesses left.";
+      }
     }
+  } else {
+    result.innerHTML = "Number is invalid! Guess a number from 1 to 20.";
   }
 }
 
+console.log(x);
 againButton.addEventListener("click", resetPage);
 function resetPage() {
   document.location.reload(true);
